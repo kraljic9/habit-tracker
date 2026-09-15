4effe13435a0aaefce050ca369346b90c4c16c17
Pavle <kraljicpavle9@gmail.com>
2026-09-15 22:04:02 +0200

    Added types

---
+++ client/src/types.ts
@@ -0,0 +1,15 @@
+
+export type BooleanMetric = {kind: 'boolean', completed: boolean};
+export type NumericMetric = {kind: 'numeric', current: number, target: number, unit: string};
+export type TimerMetric = {kind: 'timer', durationMinutes: number, targetMinutes: number};
+
+export type Metric = BooleanMetric | NumericMetric | TimerMetric;
+export type Category = 'Health' | 'Coding' | 'Fitness' | 'Mindfulness'
+
+export type Habit = {
+    id: number,
+    title: string,
+    category: Category,
+    metric: Metric,
+    streak: number
+}
