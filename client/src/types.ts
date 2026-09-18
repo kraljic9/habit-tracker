3ee9090277c87c1ffc4e1e9e4310c906228b8229
Pavle <kraljicpavle9@gmail.com>
2026-09-18 15:03:51 +0200

    Added isRunning key to TimerMetric type, changed durationMinutes to durationSeconds in TimerMetric type

--- client/src/types.ts
+++ client/src/types.ts
@@ -1,6 +1,6 @@
 export type BooleanMetric = {kind: 'boolean', completed: boolean};
 export type NumericMetric = {kind: 'numeric', current: number, target: number, unit: string};
-export type TimerMetric = {kind: 'timer', durationMinutes: number, targetMinutes: number};
+export type TimerMetric = {kind: 'timer', durationSeconds: number, targetMinutes: number, isRunning?: boolean};
 
 export type Metric = BooleanMetric | NumericMetric | TimerMetric;
 export type Category = 'Health' | 'Coding' | 'Fitness' | 'Mindfulness'
