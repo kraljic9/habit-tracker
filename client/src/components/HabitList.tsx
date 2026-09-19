5754dbfc7fe65a632069295fef685471ff4f264a
Pavle <kraljicpavle9@gmail.com>
2026-09-19 16:11:20 +0200

    Added HabitListProps interface, maped habits to display Habit Card component

---
+++ client/src/components/HabitList.tsx
@@ -0,0 +1,20 @@
+import type { Habit } from "../types";
+import { HabitCard } from "./HabitCard";
+
+interface HabitListProps {
+  habits: Habit[];
+  toggleCompleted: (id: number) => void;
+  handleIncrement: (id: number) => void;
+  handleDecrement: (id: number) => void;
+  toggleTimer: (id: number) => void;
+}
+
+export function HabitList({ habits, ...handlers }: HabitListProps) {
+  return (
+    <div className="flex flex-col gap-4">
+      {habits.map((habit) => (
+        <HabitCard key={habit.id} habit={habit} {...handlers} />
+      ))}
+    </div>
+  );
+}
