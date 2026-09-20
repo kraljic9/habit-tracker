d3c90bcfe74e7e88b98374f9fb4851fac00dac07
Pavle <kraljicpavle9@gmail.com>
2026-09-20 23:27:52 +0200

    Destructured values from useCardButtons hooks and applied to functions

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react"
-import type { Habit } from "../types";
 import HabitForm from "./HabitForm";
 import { useHabitStats } from "../hooks/useHabitStats";
 import ProgressHeader from "./ProgressHeader";
 import { HabitList } from "./HabitList";
+import { useCardButtonHooks } from "../hooks/useCardButtonHooks";
 
 const DATE_OPTIONS = {
     weekday: 'long',
@@ -14,15 +14,10 @@ const DATE_OPTIONS = {
 
 function Dashboard() {
     
-    let [habits, setHabits] = useState<Habit[]>([])
     let [isModalOpen, setIsModalOpen] = useState(false);
+    const {habits, handleAddHabit, handleDecrement, handleIncrement, toggleTimer, toggleCompleted} = useCardButtonHooks();
     
     const stats = useHabitStats(habits)
-
-    const handleAddHabit = (newHabit: Habit) => {
-        setHabits((prev) => [...prev, newHabit])
-    }
-    
     
     const [formattedDate, setFormattedDate] = useState(() => 
         new Date(Date.now()).toLocaleDateString('en-US', DATE_OPTIONS)
@@ -37,132 +32,14 @@ function Dashboard() {
 
             return () => clearInterval(interval);
         }, [])
-
-            // Habit Timer Tick Interval
-            useEffect(() => {
-                const interval = setInterval(() => {
-                    setHabits((prevHabits) =>
-                    prevHabits.map((habit) => {
-                        if (
-                        habit.metric.kind !== 'timer' ||
-                        !habit.metric.isRunning ||
-                        (habit.metric.durationSeconds ?? 0) >= habit.metric.targetMinutes * 60
-                        ) {
-                        return habit;
-                        }
-
-                    const currentSeconds = habit.metric.durationSeconds ?? 0;
-                    const nextSecond = currentSeconds + 1; 
-                    const targetSeconds = habit.metric.targetMinutes * 60;
-                    const isNewlyCompleted = nextSecond === targetSeconds;
-
-
-                    return {
-                    ...habit,
-                    streak: isNewlyCompleted ? habit.streak + 1 : habit.streak,
-                    metric: {
-                        ...habit.metric,
-                        durationSeconds: nextSecond,
-                        isRunning: !isNewlyCompleted,
-                    },
-                    };
-                })
-                );
-            }, 1000);
-
-            return () => clearInterval(interval);
-        }, []);
-        
-
-            // Functions kind increment
-
-            function handleIncrement(id : number) {
-                setHabits((prevHabits) => prevHabits.map((habit) => {
-                    if (habit.id !== id || habit.metric.kind !== 'numeric') {
-                        return habit
-                    }
-                    
-                    let nextCurrent = habit.metric.current + 1;
-                    let isNewlyCompleted = nextCurrent === habit.metric.target
-                    
-                    return {
-                        ...habit,
-                        streak: isNewlyCompleted ? habit.streak + 1 : habit.streak,
-                        metric: {
-                            ...habit.metric,
-                            current: nextCurrent
-                        }
-                    }
-                    
-                }))
-            }
-            
-            function handleDecrement(id: number) {
-                setHabits((prevHabits) => prevHabits.map((habit) => {
-                    
-                    if (habit.id !== id || habit.metric.kind !== 'numeric') {
-                        return habit
-                    }
-                    
-                    const wasCompleted = habit.metric.current >= habit.metric.target
-                    let nextCurrent = Math.max(0, habit.metric.current - 1);
-                    let isNotCompleted = wasCompleted && nextCurrent < habit.metric.target
-                    
-                    return {
-                        ...habit,
-                        streak: isNotCompleted ? Math.max(0, habit.streak - 1) : habit.streak,
-                        metric: {
-                            ...habit.metric,
-                            current: nextCurrent
-                        }
-                    }
-                }))
-            }
-            
-            // Function kind timer
-            
-            function toggleTimer(id : number) {
-                console.log("Toggling timer for ID:", id);
-                setHabits((prevHabits) => prevHabits.map((habit) => {
-                    if (habit.id !== id || habit.metric.kind !== 'timer') {
-                        return habit
-                    }
-                    
-                    return {
-                        ...habit,
-                        metric: {
-                            ...habit.metric,
-                            isRunning: !habit.metric.isRunning
-                        }
-                    }
-                }))
-            }
-            
+           
             function formatTime(totalSeconds: number) {
                 const mins = Math.floor(totalSeconds / 60);
                 const seconds = totalSeconds % 60;
                 return `${mins.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
             }
 
-            function toggleCompleted(id: number) {
-                setHabits((prevHabits) => prevHabits.map((habit) => {
-                    if (id !== habit.id || habit.metric.kind !== 'boolean') {
-                        return habit
-                    }
-
-                    let isCompleted = habit.isComplete === true;
-
-                    return {
-                        ...habit,
-                        isComplete: !habit.isComplete,
-                        streak: !isCompleted ? habit.streak + 1 : Math.max(0, habit.streak - 1),
-                        metric: {
-                            ...habit.metric,
-                            completed: !habit.metric.completed
-                        }
-                    }
-                }))
-            }
+           
 
     return (
         <div className="flex-1 min-h-screen bg-slate-950 text-slate-100 p-8">
