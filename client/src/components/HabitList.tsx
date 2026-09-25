cecd39d5a089c8fda52d774dd4387ebc5d6c3625
Pavle <kraljicpavle9@gmail.com>
2026-09-25 13:15:23 +0200

    Removed user from HabitListProps interface

--- client/src/components/HabitList.tsx
+++ client/src/components/HabitList.tsx
@@ -1,4 +1,4 @@
-import type { Habit } from "../types";
+import type { Habit, User } from "../types";
 import { HabitCard } from "./HabitCard";
 
 interface HabitListProps {
@@ -12,7 +12,7 @@ interface HabitListProps {
 export function HabitList({ habits, ...handlers }: HabitListProps) {
   return (
     <div className="flex flex-col gap-4">
-      {habits.map((habit) => (
+      {habits?.map((habit) => (
         <HabitCard key={habit.id} habit={habit} {...handlers} />
       ))}
     </div>
